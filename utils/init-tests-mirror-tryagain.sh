f020fa7e7fc5db6e1540d0aeb0573a35d21c7f11
Florent Bédécarrats <f.bedecarrats@gmail.com>
2021-12-01 22:18:45 +0100

    Trying with revers sync from container to s3

--- utils/init-tests-mirror-tryagain.sh
+++ utils/init-tests-mirror-tryagain.sh
@@ -24,6 +24,6 @@ mkdir $DOC_DIR
 
 ## copy files from S3 to container
 mc mirror s3/${S3_USER_NAME}/diffusion/test_mirror $DATA_DIR
-## mirror from container to S3
-# mc mirror --watch $DATA_DIR s3/${S3_USER_NAME}/diffusion/test_mirror
+# mirror from container to S3
+mc mirror --watch $DATA_DIR s3/${S3_USER_NAME}/diffusion/test_mirror
 
