4bdc82313878ba717dd1c65111324bb69553e531
Florent Bédécarrats <f.bedecarrats@gmail.com>
2021-12-01 21:49:39 +0100

    Setting a test environment

---
+++ utils/init-tests-mirror-3.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Create variables
+## Variable parameters: to ajust manually
+PROJ_NAME=test_mirror_s3
+GITHUB_USER_MAIN=fBedecarrats
+S3_USER_NAME=fbedecarrats
+
+## Variables: 
+WORK_DIR=/home/rstudio/${PROJ_NAME}
+REPO_URL=https://github.com/${GITHUB_USER_MAIN}/${PROJ_NAME}
+DATA_DIR=${WORK_DIR}/data
+DOC_DIR=${WORK_DIR}/documentation
+
+# Git
+git clone $REPO_URL $WORK_DIR
+chown -R rstudio:users $WORK_DIR
+
+# Folders to store data and documentation
+mkdir $DATA_DIR
+mkdir $DOC_DIR
+
+
+
+## copy files from S3 to container
+mc cp $DATA_DIR s3/${S3_USER_NAME}/diffusion/test_mirror $DOC_DIR
+## mirror from container to S3
+mc mirror --watch $DATA_DIR s3/${S3_USER_NAME}/diffusion/test_mirror
+
