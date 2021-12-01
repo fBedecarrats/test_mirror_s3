9f76e533309577acc4c707e12f0cd15ce8215307
Florent Bédécarrats <f.bedecarrats@gmail.com>
2021-12-01 22:48:01 +0100

    chown solved it with overwrite, let's try with watch

--- utils/init-tests-mirror-tryagain.sh
+++ utils/init-tests-mirror-tryagain.sh
@@ -26,5 +26,5 @@ chown -R rstudio:users $DATA_DIR
 ## copy files from S3 to container
 mc cp s3/${S3_USER_NAME}/diffusion/test_mirror/commune2021.csv $DATA_DIR
 # mirror from container to S3
-mc mirror --overwrite $DATA_DIR s3/${S3_USER_NAME}/diffusion/test_mirror
+mc mirror --watch $DATA_DIR s3/${S3_USER_NAME}/diffusion/test_mirror
 
