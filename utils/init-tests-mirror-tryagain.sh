d5309b95b9ad66de3a2681b3873e315aa1305638
Florent Bédécarrats <f.bedecarrats@gmail.com>
2021-12-02 10:30:48 +0100

    Back to where it half worked

--- utils/init-tests-mirror-tryagain.sh
+++ utils/init-tests-mirror-tryagain.sh
@@ -26,5 +26,5 @@ chown -R rstudio:users $DATA_DIR
 ## copy files from S3 to container
 mc cp s3/${S3_USER_NAME}/diffusion/test_mirror/commune2021.csv $DATA_DIR
 # mirror from container to S3
-mc mirror --watch $DATA_DIR s3/${S3_USER_NAME}/write
+mc mirror --watch $DATA_DIR s3/${S3_USER_NAME}/diffusion/test_mirror/
 
