47e70584d419d77d6cb92918f5bddd15aa974ba5
Florent Bédécarrats <f.bedecarrats@gmail.com>
2021-12-02 18:41:40 +0100

    Simplifying a little

--- utils/init-tests-mirror-tryagain.sh
+++ utils/init-tests-mirror-tryagain.sh
@@ -10,7 +10,6 @@ S3_USER_NAME=fbedecarrats
 WORK_DIR=/home/rstudio/${PROJ_NAME}
 REPO_URL=https://github.com/${GITHUB_USER_MAIN}/${PROJ_NAME}
 DATA_DIR=${WORK_DIR}/data
-DOC_DIR=${WORK_DIR}/documentation
 
 # Git
 git clone $REPO_URL $WORK_DIR
@@ -18,13 +17,10 @@ chown -R rstudio:users $WORK_DIR
 
 # Folders to store data and documentation
 mkdir $DATA_DIR
-mkdir $DOC_DIR
 
 chown -R rstudio:users $DATA_DIR
-
 
 ## copy files from S3 to container
 mc cp s3/${S3_USER_NAME}/diffusion/test_mirror/commune2021.csv $DATA_DIR
 # mirror from container to S3
 mc mirror --watch $DATA_DIR s3/${S3_USER_NAME}/diffusion/test_mirror/
-
